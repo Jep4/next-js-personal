5c2947e4ba8a06e3c4d172a7013e3bd5a828a27f
Jooeun Park <civbf1@gmail.com>
2024-05-04 22:51:51 -0700

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container w-96">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>0000...</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className='half-visible'>0000IN0</div>
+      <main >
+        <p className='pt-4 pb-2'>이곳에 기재된 닉네임으로 불러주세요 | 인</p>
+        <hr style={{ border: "1px #444 solid", marginBottom: "3px" }} />
+        
+        <div>
+          <p style={{ marginTop: "5px" }}> 텍스트 텍스트 </p>
+          <hr style={{ border: "1px #e4e4e4 solid"}} />
+          <p style={{ marginTop: "5px" }}>텍스트</p>
+        </div>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <hr style={{ border: "1px #e4e4e4 solid" }} />
       </main>
 
-      <Footer />
     </div>
   )
 }
